e72cb04b9a95d2b2e3c38d7595ce80f6a7fdb12c
Tristan Mayfield <Tristan.Mayfield@sophos.com>
2022-01-06 09:59:54 -0700

    first half of day one and day two

--- 1/main.c
+++ 1/main.c
@@ -36,6 +36,22 @@ In this example, there are 7 measurements that are larger than the previous meas
 How many measurements are larger than the previous measurement?
 */
 
+/*
+ Part 2
+ Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
