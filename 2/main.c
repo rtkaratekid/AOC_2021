ff3e978b6dd0366785f6479deaca1b2eb55767fa
Tristan Mayfield <Tristan.Mayfield@sophos.com>
2022-01-06 10:05:23 -0700

    formatting

--- 2/main.c
+++ 2/main.c
@@ -1,53 +1,22 @@
-/*
-It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
-
-forward X increases the horizontal position by X units.
-down X increases the depth by X units.
-up X decreases the depth by X units.
-Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you might expect.
-
-The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going. For example:
-
-forward 5
-down 5
-forward 8
-up 3
-down 8
-forward 2
-Your horizontal position and depth both start at 0. The steps above would then modify them as follows:
-
-forward 5 adds 5 to your horizontal position, a total of 5.
-down 5 adds 5 to your depth, resulting in a value of 5.
-forward 8 adds 8 to your horizontal position, a total of 13.
-up 3 decreases your depth by 3, resulting in a value of 2.
-down 8 adds 8 to your depth, resulting in a value of 10.
-forward 2 adds 2 to your horizontal position, a total of 15.
-After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
-
-Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
-
 int main(int argc, char **argv) {
 
-	FILE * fp;
-	char * line = NULL;
-	size_t len = 0;
-	ssize_t read;
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
-	fp = fopen(argv[1], "r");
-	if (fp == NULL) {
-		exit(EXIT_FAILURE);
-	}
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        exit(EXIT_FAILURE);
+    }
 
-	int depth = 0;
-	int horizontal = 0;
-	while ((read = getline(&line, &len, fp)) != -1) {
+    int depth = 0;
+    int horizontal = 0;
+    while ((read = getline(&line, &len, fp)) != -1) {
         line[ strlen(line) - 1 ] = '\0'; // drop newline
 
         //char *saveptr;
@@ -69,7 +38,6 @@ int main(int argc, char **argv) {
     }
 
     printf("Depth: %d\nHorizontal: %d\nMulitplied: %d\n", depth, horizontal, depth*horizontal);
-	
 
     return 0;
 }
