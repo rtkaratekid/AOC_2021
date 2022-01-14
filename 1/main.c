5e4232e2abdc80bab40a3dafd8c3a3e788d3dfae
Tristan Mayfield <Tristan.Mayfield@sophos.com>
2022-01-14 09:20:18 -0700

    formatting for my ego

--- 1/main.c
+++ 1/main.c
@@ -20,7 +20,7 @@ void slide_window(window *w, int next_line) {
 int main(int argc, char **argv) {
 
 	if (argc != 2) {
-			printf("Not enough arguments provided\n");
+        printf("Not enough arguments provided\n");
 	}
 
 	FILE * fp;
