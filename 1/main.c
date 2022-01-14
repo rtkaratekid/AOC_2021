6d11c97393e3df0d8c25eeeb3b2960da10e8aecb
Tristan Mayfield <Tristan.Mayfield@sophos.com>
2022-01-14 09:18:27 -0700

    day one part two

--- 1/main.c
+++ 1/main.c
@@ -1,60 +1,21 @@
-/*
-As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor. On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
-
-For example, suppose you had the following report:
-
-199
-200
-208
-210
-200
-207
-240
-269
-260
-263
-
-This report indicates that, scanning outward from the submarine, the sonar sweep found depths of 199, 200, 208, 210, and so on.
-
-The first order of business is to figure out how quickly the depth increases, just so you know what you're dealing with - you never know if the keys will get carried into deeper water by an ocean current or a fish or something.
-
-To do this, count the number of times a depth measurement increases from the previous measurement. (There is no measurement before the first measurement.) In the example above, the changes are as follows:
-
-199 (N/A - no previous measurement)
-200 (increased)
-208 (increased)
-210 (increased)
-200 (decreased)
-207 (increased)
-240 (increased)
-269 (increased)
-260 (decreased)
-263 (increased)
-
-In this example, there are 7 measurements that are larger than the previous measurement.
-
-How many measurements are larger than the previous measurement?
-*/
-
-/*
- Part 2
- Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
-
-In the above example, the sum of each three-measurement window is as follows:
-
-A: 607 (N/A - no previous sum)
-B: 618 (increased)
-C: 618 (no change)
-D: 617 (decreased)
-E: 647 (increased)
-F: 716 (increased)
-G: 769 (increased)
-H: 792 (increased)
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct {
+    int first;
+    int second;
+    int third;
+    int total;
+}window;
+
+void slide_window(window *w, int next_line) {
+    w->first = w->second;
+    w->second = w->third;
+    w->third = next_line;
+    w->total = w->first + w->second + w->third;
+}
+
 
 int main(int argc, char **argv) {
 
@@ -72,16 +33,22 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
-	int prev = 0;
+    window curr_w = {0,0,0};
+	int prev_window_total = 0;
 	int count = 0;
+
 	while ((read = getline(&line, &len, fp)) != -1) {
 		line[ strlen(line) - 1 ] = '\0'; // drop newline
 		int line_num = atoi(line); // convert to int
-		if (line_num > prev) count++; // count
-		prev = line_num;
+
+        slide_window(&curr_w, line_num);
+
+		if (curr_w.total > prev_window_total) count++; // count
+		prev_window_total = curr_w.total;
     }
 
-	count--; // account for starting loop edge case
+    // hacky way to account for starting loops edge case without reading the file from the third line
+	count -= 3;
 	printf("number of larger measurements is %d\n", count);
 
 	return 0;
